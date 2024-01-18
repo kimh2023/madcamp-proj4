4385930f419a147548d7da3387e97ad033e10fc5
Hyun A Kim <hyuna@Violet.local>
2024-01-18 22:10:43 +0900

    chore: eslint, prettier, threejs

---
+++ src/pages/pure-three.tsx
@@ -0,0 +1,45 @@
+import { Canvas } from "@react-three/fiber";
+import { useEffect } from "react";
+import * as THREE from "three";
+
+const Tester = () => {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      typeof window !== "undefined"
+        ? window.innerWidth / window.innerHeight
+        : 0,
+      0.1,
+      1000,
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xfff });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+    animate();
+
+    console.log(scene);
+  }, []);
+
+  return (
+    <Canvas>
+      <mesh />
+    </Canvas>
+  );
+};
+export default Tester;
---
+++ src/pages/tester.tsx
@@ -0,0 +1,10 @@
+import { Canvas } from "@react-three/fiber";
+
+const Tester = () => {
+  return (
+    <Canvas>
+      <mesh />
+    </Canvas>
+  );
+};
+export default Tester;
