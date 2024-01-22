42fb7470248af93a5ba5be75c5d87a72d810d682
Hyun A Kim <hyuna@Violet.local>
2024-01-22 21:51:37 +0900

    feat: legs and arms animation when moving

--- src/components/todoComponents/canvas/TodoCanvas.tsx
+++ src/components/todoComponents/canvas/TodoCanvas.tsx
@@ -1,86 +1,40 @@
-import { Canvas, ThreeEvent, useThree } from "@react-three/fiber";
+import { Canvas, ThreeEvent } from "@react-three/fiber";
 import { Layout } from "antd";
 import React, { Suspense, lazy, useRef, useState } from "react";
 import NotFound from "../../styledComponents/NotFound";
-import { Mesh, Vector2, Vector3 } from "three";
-import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Mesh, Vector3 } from "three";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
 const Rabbit = lazy(() => import("./models/Rabbit"));
 
 const TodoCanvasInner = () => {
   const rabbitRef = useRef<Mesh>(null);
   const [rabbitPosition, setRabbitPosition] = useState<Vector3>(
-    new Vector3(0, 0, 1),
+    new Vector3(0, 0, 0),
   );
-  const { raycaster, size, scene } = useThree();
-  const camera = useThree((state) => state.camera);
 
   const handleCanvasClick = (event: ThreeEvent<MouseEvent>) => {
-    console.log(rabbitRef, event);
-    if (rabbitRef.current === null) {
-      return;
-    }
-    const { clientX, clientY } = event;
-    const mouseX = (clientX / window.innerWidth) * 2 - 1;
-    const mouseY = (clientY / window.innerHeight) * 2 - 1;
-
-    const mouse = new Vector2(mouseX, mouseY);
-    raycaster.setFromCamera(mouse, camera);
-    // raycaster.near = 0.1; // Adjust the near value
-    // raycaster.far = 1000;
-    // console.log(raycaster.far, raycaster.near);
-    console.log(
-      clientX,
-      window.innerWidth,
-      (clientX / window.innerWidth) * 2 - 1,
-    );
-    console.log(
-      clientY,
-      window.innerHeight,
-      (clientY / window.innerHeight) * 2 - 1,
-    );
-
-    console.log(
-      "CHECK RAYCASTER",
-      raycaster.ray.origin,
-      raycaster.ray.direction,
-      rabbitRef.current,
-    );
-
-    // const intersects = raycaster.intersectObject(rabbitRef.current, true);
-    const intersects = raycaster.intersectObjects(scene.children, true);
-    console.log(intersects);
-
-    // if (intersects.length > 0) {
-    //   setRabbitPosition(
-    //     new Vector3(intersects[0].point.x, intersects[0].point.y, 0),
-    //   );
-    //   console.log(intersects[0].point);
-    // }
-    setRabbitPosition(new Vector3(event.point.x, event.point.y, 0));
+    setRabbitPosition(new Vector3(event.point.x, 0, event.point.z));
   };
 
   return (
     <React.Fragment>
-      {/* <perspectiveCamera
-        position={[0, 5, 5]}
-        fov={75}
-        aspect={size.width / size.height}
-        near={0.1}
-        far={1000}
-      /> */}
-
-      <directionalLight position={[0, 0, 5]} />
-      {/* <Rabbit
-        ref={rabbitRef}
+      <Rabbit
         position={rabbitPosition}
+        rotation={[0, 0, 0]}
         onClick={() => console.log("hi")}
-      /> */}
-      <mesh ref={rabbitRef} position={rabbitPosition}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial />
-      </mesh>
-      <mesh position={[0, 0, -1]} onClick={handleCanvasClick}>
+      />
+
+      <mesh
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, -0.2, 0]}
+        onClick={handleCanvasClick}
+      >
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial color="gray" />
       </mesh>
@@ -94,15 +48,17 @@ const TodoCanvas = () => {
       <Suspense fallback={<NotFound />}>
         <Canvas>
           <Environment preset="sunset" />
-          <ambientLight intensity={1} />
+          <ambientLight intensity={1} castShadow />
           <OrbitControls />
-          <ContactShadows blur={1} />
-          <perspectiveCamera
-            position={[0, 0, 10]}
-            fov={75}
-            near={0}
-            far={Infinity}
+          <ContactShadows
+            opacity={0.5}
+            scale={11}
+            blur={5}
+            near={-1}
+            far={30}
+            resolution={512}
           />
+          <PerspectiveCamera makeDefault position={[10, 10, 10]} />
           <TodoCanvasInner />
         </Canvas>
       </Suspense>
--- src/components/todoComponents/canvas/models/Rabbit.tsx
+++ src/components/todoComponents/canvas/models/Rabbit.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useGLTF } from "@react-three/drei";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 import { MeshProps, useFrame } from "@react-three/fiber";
 
@@ -7,24 +7,52 @@ interface RabbitProps extends MeshProps {}
 
 const Rabbit = ({ ...props }: RabbitProps) => {
   const model = useGLTF(`${process.env.FRONTEND_URL}/models/rabbit8.glb`);
+  const mesh =
+    useRef<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>>>(
+      null,
+    );
+  const [animation, setAnimation] = useState("walk");
+  const position = useMemo(() => props.position, []);
+  const { actions } = useAnimations(model.animations, mesh);
+
   const modelAnimations = model.animations;
   console.log(modelAnimations);
   const mixer = new THREE.AnimationMixer(model.scene);
 
   model.animations.forEach((clip) => {
-    if (clip.name === "walk1" || clip.name === "walk2") {
+    if (clip.name === "walk1") {
       const action = mixer.clipAction(clip);
       action.play();
-      console.log(clip);
     }
   });
 
-  useFrame((state, delta) => {
-    mixer?.update(delta);
+  useEffect(() => {
+    actions[animation]?.reset().fadeIn(0.5).play();
+    return (): void => {
+      actions[animation]?.fadeOut(0.5);
+    };
+  }, [animation]);
+
+  useFrame(() => {
+    if (
+      mesh?.current &&
+      mesh?.current.position.distanceTo(props.position as THREE.Vector3) > 0.1
+    ) {
+      const direction = mesh.current.position
+        .clone()
+        .sub(props.position as THREE.Vector3)
+        .normalize()
+        .multiplyScalar(0.1);
+      mesh.current.position.sub(direction);
+      mesh.current.lookAt(props.position as THREE.Vector3);
+      setAnimation("walk2");
+    } else {
+      setAnimation("");
+    }
   });
 
   return (
-    <mesh {...props}>
+    <mesh ref={mesh} {...props} position={position}>
       <primitive object={model.scene} />
     </mesh>
   );
